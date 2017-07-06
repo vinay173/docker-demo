d454563d94dbfee2db34559d6f8bcc2d20403326
deraviyam <deraviyam@gmail.com>
2017-07-06 15:05:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(request, response){
+    response.sendfile('homepage.html');
 });
 
 var server = app.listen(3000, function () {
